92cc8bf7bc25da8edd9cec5279c52b64f2d161cc
TomasGomez02 <tagomez@estudiantes.unsam.edu.ar>
2023-09-22 17:14:28 -0300

    Arraylist

--- 02_herencia/src/viajes/Main.java
+++ 02_herencia/src/viajes/Main.java
@@ -1,17 +1,25 @@
 package viajes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main 
 {
     public static void main(String args[])
     {
         Trayecto bariloche = new Trayecto("CABA", "Bariloche", 1500, 5);
         Viaje barilocheDiesel = new ViajeDiesel(bariloche, 10, 500);
-        System.out.println(barilocheDiesel.tiempoDeDemora());
-
         Viaje barilocheElectrico = new ViajeElectrico(bariloche, 10, 500);
-        System.out.println(barilocheElectrico.tiempoDeDemora());
-
         Viaje barilocheAltaVelocidad = new ViajeAltaVelocidad(bariloche, 10, 500);
-        System.out.println(barilocheAltaVelocidad.tiempoDeDemora());
+
+        List<Viaje> viajes = new ArrayList<Viaje>();
+        viajes.add(barilocheDiesel);
+        viajes.add(barilocheElectrico);
+        viajes.add(barilocheAltaVelocidad);
+        
+        for (Viaje viaje: viajes)
+        {
+            System.out.println(viaje.tiempoDeDemora());
+        }
     }    
 }
